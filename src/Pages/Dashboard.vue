9ed74e3fe3255f62e030845262511365835568f1
henry <henryboy753951@gmail.com>
2023-10-17 03:09:15 +0800

    wrap InfoPanel component

--- src/Pages/Dashboard.vue
+++ src/Pages/Dashboard.vue
@@ -4,10 +4,25 @@
 	const { icpu, igpu } = useMonitoring();
 </script>
 
+<script lang="ts">
+	let panel: string = "dual data";
+</script>
+
 <template>
-	<div>
-		<p class="text-3xl font-bold">Dashboard</p>
-		<InfoPanel :icpu="icpu" :igpu="igpu" />
+	<div class="max-w-850px w-screen p-3">
+		<p class="text-3xl font-bold text-center">Dashboard</p>
+		<div class="flex">
+			<div class="bg-dark-800 rounded-3xl m-2">
+				<InfoPanel :icpu="icpu" :igpu="igpu" />
+			</div>
+
+			<div class="w-full">
+				<select v-model="panel">
+					<option value="dual_data">雙資訊</option>
+					<option value="current_song">Cider</option>
+				</select>
+			</div>
+		</div>
 	</div>
 </template>
 
--- src/components/InfoPanel.vue
+++ src/components/InfoPanel.vue
@@ -13,23 +13,25 @@
 </script>
 
 <template>
-	<div class="w-200px">
-		<p class="text-lg text-$color-primary">CPU</p>
-		<div class="flex flex-col justify-start">
-			<div>
-				Temperature:
-				{{ Math.round(((icpu?.temperature as number) * 10) / 10) }}°C
+	<div class="w-180px flex flex-col items-center my-3">
+		<div>
+			<p class="text-lg text-$color-primary">CPU</p>
+			<div class="flex flex-col justify-start">
+				<div>
+					Temperature:
+					{{ Math.round(((icpu?.temperature as number) * 10) / 10) }}°C
+				</div>
+				<div>Load: {{ Math.round((icpu?.load as number) * 1000) / 10 }}%</div>
 			</div>
-			<div>Load: {{ Math.round((icpu?.load as number) * 1000) / 10 }}%</div>
-		</div>
 
-		<p class="text-lg text-$nzxt-color">GPU</p>
-		<div class="flex-row">
-			<div>
-				Temperature:
-				{{ Math.round(((igpu?.temperature as number) * 10) / 10) }}°C
+			<p class="text-lg text-$nzxt-color">GPU</p>
+			<div class="flex-row">
+				<div>
+					Temperature:
+					{{ Math.round(((igpu?.temperature as number) * 10) / 10) }}°C
+				</div>
+				<div>Load: {{ Math.round((igpu?.load as number) * 1000) / 10 }}%</div>
 			</div>
-			<div>Load: {{ Math.round((igpu?.load as number) * 1000) / 10 }}%</div>
 		</div>
 	</div>
 </template>
